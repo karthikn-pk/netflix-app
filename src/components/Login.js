97a7cc07dd6e6d8d4fb39c9c456654f9de797576
karthikn-pk <pkarthikeyanp33@gmail.com>
2024-03-25 11:50:31 +0530

    form validation completed

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,15 +4,19 @@ import { checkValidateData } from '../utils/validate';
 
 const Login = () => {
     const[isSignInForm,setIsSignInForm]=useState(true);
+    const[errorMessage,setErrorMessage]=useState(null);
 
     const email=useRef(null);
     const password=useRef(null);
+    const name=useRef(null);
 
     const handleButtonClick=()=>{
+      const nameValue=name.current.value;
       const emailValue = email.current.value; 
     const passwordValue = password.current.value;
-      const message=checkValidateData(emailValue,passwordValue);
-      console.log(message);
+      const message=checkValidateData(nameValue,emailValue,passwordValue);
+      console.log(nameValue);
+      setErrorMessage(message);
 
     }
     
@@ -30,10 +34,11 @@ const Login = () => {
         </div>
         <form onSubmit={(e)=>{ e.preventDefault();}} className='w-3/12  rounded-md absolute p-10 mx-auto my-36 right-0 left-0 bg-black text-white bg-opacity-80'>
             <h1 className='text-white p-4 font-bold text-3xl'>{isSignInForm ? "Sign In" : "Sign Up"}</h1>
-            {!isSignInForm && <input type='text' placeholder='Full Name' className='bg-black w-full border border-white border-solid  m-2 p-2'/>}
+            {!isSignInForm && <input ref={name} type='text' placeholder='Full Name' className='bg-black w-full border border-white border-solid  m-2 p-2'/>}
 
             <input ref={email} type='text' placeholder='Email Address' className='bg-black w-full border border-white border-solid  m-2 p-2'/>
             <input ref={password} type='password' placeholder='Password' className='bg-black w-full border border-white border-solid m-2 p-2'/>
+            <p className='text-red-600 font-bold text-lg p-2'>{errorMessage}</p>
             <button className='w-full  rounded-lg p-4 m-4  bg-red-700 text-center font-bold' onClick={handleButtonClick}>{isSignInForm ? "Sign In" : "Sign Up"}</button>
             <p className='my-4 cursor-pointer' onClick={toggleSignup}>{isSignInForm ? "New to Netflix? Sign up now!" : "Already have an account? Sign In" }</p>
 
--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,10 +1,12 @@
-export const checkValidateData=(email,password)=>{
+export const checkValidateData=(name,email,password)=>{
 
+    const isNameValid=/^[0-9A-Za-z]{6,16}$/.test(name);
     const isEmailValid=/^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/.test(email);
     const isPasswordValid=/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
 
     if(!isEmailValid) return "Email is not valid";
     if(!isPasswordValid) return "Password is not valid";
+    if(!isNameValid) return "UserName is not valid"
 
     return null;
 };
